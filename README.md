db18310c6702dcbb1a69e241f423f83a201f2fc5
Pigeon <pigeon4455@protonmail.com>
2022-10-01 16:46:47 +0100

    release

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ShiroHook
- A simple C# application to ping ShyShiro every 30 minutes
+ A simple C# application to ping ShyShiro every 30 minutes through a discord webhook
+
+## How to use
+
+Compile as release, create a file named exactly "config" containing the link to the webhook
+Run "Shiro.exe" and it should ping on launch and every 30 minutes
---
+++ Shiro/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Shiro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(30);
+
+            Console.WriteLine("Started ShiroHook\n");
+            var timer = new System.Threading.Timer((e) =>
+            {
+                discord.Send("<@868507165730676757>");
+            }, null, startTimeSpan, periodTimeSpan);
+            Console.ReadLine();
+        }
+    }
+    public static class discord
+    {
+        public static class Http
+        {
+            public static byte[] Post(string url, NameValueCollection pairs)
+            {
+                using (WebClient webClient = new WebClient())
+                    return webClient.UploadValues(url, pairs);
+            }
+        }
+        public static void Send(string content)
+        {
+            Console.WriteLine("Sent Ping: " + DateTime.Now);
+            string webHookUrl = File.ReadAllText("config");
+            Http.Post(webHookUrl, new NameValueCollection()
+            {
+                {
+                    "content", content
+                },
+
+                {
+                    "username", "ShiroHook"
+                },
+
+                {
+                    "avatar_url", "https://steamuserimages-a.akamaihd.net/ugc/1808779657407939622/0163A125DE2A4A805915E6F24712756AA14D1E49/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
+                }
+
+            });
+        }
+    }
+}
